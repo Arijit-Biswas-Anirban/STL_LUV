4174121d88d1b90188fb7b48c3282886d31af986
Arijit-Biswas-Anirban <arijit.cse.pust27@gmail.com>
2024-05-01 22:33:19 +0800

    Adding merge sort

---
+++ _27_Merge_Sort.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define For(i,a,n) for(int i=a; i<n; i++)
+#define in cin>>
+#define srt(a) sort(a.begin(), a.end());
+#define vtr vector<long long>
+#define unset unordered_set<long long>
+#define pr cout<<
+#define nl <<endl;
+using namespace std;
+
+const int N = 1e5+7;
+vtr vec;
+int arr[N];
+
+void merge(int l, int r, int mid){
+    int l_size = mid - l + 1;
+    int r_size = r - (mid+1) + 1;
+    int L_arr[l_size + 1];
+    int R_arr[r_size + 1];
+}
+
+void anuhimu(){
+
+}
+int main(){
+// ll tc;
+// in tc;
+// while(tc--){
+//     anuhimu();
+// }
+
+int size;   in size;
+    int arr[size];
+
+    For(i,0,size){
+        in arr[i];
+    }
+
+
+return 0;
+}
