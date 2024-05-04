1c12d4a4a6b9a2531c57646fd117c886a6a35ab8
Arijit-Biswas-Anirban <arijit.cse.pust27@gmail.com>
2024-05-04 20:54:00 +0800

    adding how to find and count total connected component in a graph

--- _85_find_connected_components_cycle_inGraph_dfs.cpp
+++ _85_find_connected_components_cycle_inGraph_dfs.cpp
@@ -17,20 +17,23 @@ using namespace std;
 const int N = 1e5 + 7;
 vtr graph[N];
 bool visited[N];
+vector<vector<int>> connected_component;
+vector<int> currently_connectedComponent;
 
 void dfs(int vertex)
 {
     /**
      * Take action on vertex after entering the vertex
      */
-    p "Visiting Vertex : " << vertex nl
+    // p "Visiting Vertex : " << vertex nl
     visited[vertex] = true;
+    currently_connectedComponent.push_back(vertex);
     for (int child : graph[vertex])
     {
         /**
          * Take action on child before entering the child node
          */
-        p "Parent vertex: "<< vertex << " "<<"Child node: " << child nl
+        // p "Parent vertex: "<< vertex << " "<<"Child node: " << child nl
         if (visited[child])
             continue;
         dfs(child);
@@ -53,11 +56,12 @@ int main()
     // while(tc--){
     //     anu();
     // }
-    
+
     // Initialize visited array to false
     memset(visited, false, sizeof(visited));
 
-    int n, m;
+    int n, m, cnt = 0;
+    ;
     in n >> m;
     For(i, 0, m)
     {
@@ -66,30 +70,42 @@ int main()
         graph[v1].push_back(v2);
         graph[v2].push_back(v1);
     }
-    dfs(1);
 
     // Optionally, iterate through all vertices and call DFS on each unvisited vertex
     // This is useful for disconnected graphs
-    // For_eq(i,1,n)
-    // {
-    //     if(!visited[i]){
-    //         dfs(i);
-    //     }
-    // }
+    For_eq(i, 1, n)
+    {
+        if (!visited[i])
+        {
+            currently_connectedComponent.clear();
+            dfs(i);
+            connected_component.push_back(currently_connectedComponent);
+            cnt++;
+        }
+    }
+
+    p cnt nl 
+    for (auto it : connected_component)
+    {
+        for (int vertex : it)
+        {
+            p vertex << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
 
 /*
-    6 9
+    Qs. Find total number of connected components in the graphs.
+    ***A connected component is a set of vertices in a graph that are linked to each other by paths(edges)***
 
-    1 3
-    1 5
+    8 5
+
+    1 2
+    2 3
+    2 4
     3 5
-    3 4
-    3 6
-    3 2
-    2 6
-    4 6
-    5 6
+    6 7
 
 */
